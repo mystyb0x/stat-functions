0a7b3409d30ed9cb88370c59cb4c043fcf8e07ce
Triston <achuku@augusta.edu>
2025-10-29 17:19:28 -0400

    created stdev functions + updated mean functions

--- code/functions.py
+++ code/functions.py
@@ -1,3 +1,5 @@
+import math
+
 def mean_binomial_dist(p: float, n: int) -> float:
     """Calculates the mean of a binomial distribution.
     
@@ -15,6 +17,9 @@ def mean_binomial_dist(p: float, n: int) -> float:
     
     if not isinstance(n, int):
         raise ValueError("n must be a non-zero positive integer.")
+    
+    if n <= 0:
+        raise ValueError("n must be a non-zero positive integer.")
         
     return n * p
 
@@ -29,7 +34,7 @@ def mean_geometric_dist(p: float, support = 0) -> float:
     :type support: int
     :return: The mean of the geometrical distribution.
     :rtype: float
-    :raises ValueError: if p is not between 0 < p <= 1.
+    :raises ValueError: if p does not agree with 0 < p <= 1.
     :raises ValueError: if support is not 0 or 1. 
     """
     if not (0 < p <= 1):
@@ -53,21 +58,106 @@ def mean_uniform_dist(a: int | float, b: int | float) -> float:
     :rtype: float
     :raises ValueError: if a and b do not agree with a < b.
     """
-    if not a < b:
+    if a > b:
         raise ValueError("a and b must agree with a < b.")
     
     return (a * b) / 2
 
-def mean_exponential_dist(λ: float) -> float:
+def mean_exponential_dist(λ: int | float) -> float:
     """Calculates the mean of an exponential distribution.
     
     :param λ: The rate parameter. 
-    :type λ: float
+    :type λ: int | float
     :return: The mean of the exponential distribution.
     :rtype: float
-    :raises ValueError: if λ is equal to 0.
+    :raises ValueError: if λ is less than or equal to 0.
     """
-    if λ == 0:
-        raise ValueError("λ cannot be equal to 0.")
+    if λ <= 0:
+        raise ValueError("λ cannot be less than or equal to 0.")
     
     return 1 / λ
+
+def stdev_binomial_dist(p: float, n: int) -> float:
+    """Calculates the standard deviation of a binomial distribution.
+
+    :param p: The p-value.
+    :type p: float
+    :param n: The sample size.
+    :type n: int
+    :return: The standard deviation of the binomial distribution.
+    :rtype: float
+    :raises ValueError: if p does not agree with 0 <= p <= 1.
+    :raises ValueError: if n is not a non-zero positive integer.
+    """
+    if not (0 <= p <= 1):
+        raise ValueError("p must agree with 0 <= p <= 1.")
+    
+    if not isinstance(n, int):
+        raise ValueError("n must be a non-zero positive integer.")
+    
+    if n <= 0:
+        raise ValueError("n must be a non-zero positive integer.")
+    
+    var = n * p * (1-p)
+    return math.sqrt(var)
+
+def stdev_geometric_dist(p: float) -> float:
+    """Calculates the standard deviation of a geometric distribution.
+    
+    :param p: The p-value.
+    :type p: float
+    :return: The standard deviation of the geometric distribution.
+    :rtype: float
+    :raises ValueError: if p is not between 0 < p <= 1.
+    """
+    if not (0 < p <= 1):
+        raise ValueError("p must agree with 0 < p <= 1.")
+    
+    var = (1-p) / (p ** 2)
+    return math.sqrt(var)
+
+def stdev_poisson_dist(λ: int | float) -> float:
+    """Calculates the standard deviation of a poisson distribution.
+
+    :param λ: The mean.
+    :type λ: int | float
+    :return: The standard deviation of the poisson distribution.
+    :rtype: float
+    :raises ValueError: if λ is less than or equal to 0.
+    """
+    if λ <= 0:
+        raise ValueError("λ cannot be less than or equal to 0.")
+    
+    return math.sqrt(λ)
+
+def stdev_uniform_dist(a: int | float, b: int | float) -> float:
+    """Calculates the standard deviation of a uniform distribution.
+
+    :param a: The lower bound
+    :type a: int | float
+    :param b: The upper bound
+    :type b: int | float
+    :return: The standard deviation of a uniform distribution.
+    :rtype: float
+    :raises ValueError: if a and b do not agree with a < b.
+    """
+    if a > b:
+        raise ValueError("a and b must agree with a < b.")
+    
+    var = (1/12) * ((b - a) ** 2)
+    return math.sqrt(var)
+
+def stdev_exponential_dist(λ: int | float) -> float:
+    """Calculates the standard deviation of a exponential distribution.
+
+    :param λ: The decay meter
+    :type λ: int | float
+    :return: The standard deviation of the exponential distribution.
+    :rtype: float
+    :raises ValueError: if λ is less than or equal to 0.
+    """
+    if λ <= 0:
+        raise ValueError("λ cannot be less than or equal to 0.")
+    
+    var = 1 / (λ ** 2)
+    return math.sqrt(var)
